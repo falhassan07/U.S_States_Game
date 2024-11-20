21af58b954869dad724bcf5955b00ba0031eb871
Farouk Alhassan <omarfarouk@farouks-mbp.home>
2024-11-19 22:43:55 -0500

    added missed state functionality

--- main.py
+++ main.py
@@ -12,8 +12,38 @@ screen.addshape(image)
 turtle.shape(image)
 
 
-user_ans = screen.textinput("Guess the state", "What's another state's name?")
+guessed_states = []
+missed_states = []
 
+while len(guessed_states) < 50:
+    user_ans = (screen.textinput(f"{len(guessed_states)}/50", "What's another state's name?")).title()
+
+
+    # user_ans = input("What is the state:")
+    states_data = pandas.read_csv("50_states.csv")
+    states = states_data["state"].to_list()
+    # print(states)
+
+    if user_ans == "Exit":
+        for state in states:
+            if state not in guessed_states:
+                missed_states.append(state)
+
+        df = pandas.DataFrame(missed_states)
+        df.to_csv("missed_states.csv")
+        break
+
+    if user_ans in states:
+        guessed_states.append(user_ans)
+        t = Turtle()
+        t.penup()
+        t.hideturtle()
+        data = states_data[states_data["state"] == user_ans]
+        x = data["x"]
+        y = data["y"]
+        t.goto(x.item(),y.item())
+        t.write(user_ans, font=("Arial", 20, "normal"))
+        
 
 
 
